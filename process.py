cb84718aeea57f5555272c4290c5ded063a3e3b6
Shu <shuhanb@gmail.com>
2014-04-19 14:41:42 +1000

    change implementation of corpus from list to dictionary

--- process.py
+++ process.py
@@ -3,67 +3,45 @@ import gzip
 import string
 
 path = 'lingspam-mini600'
-subj = []
-body = []
 
-# subj_corpus = {}
-# body_corpus = {}
-#
-# for file in os.listdir(path):
-# 	f = gzip.open(os.path.join(path, file), 'rb')
-# 	try:
-# 		for line in f:
-# 			for word in line.split():
-# 				# Line contains subject
-# 				if line.startswith('Subject:'):
-#					# Increment or initialise item
-# 					subj_corpus[word] = subj_corpus(word, 0) + 1
-# 				# Line contains body
-# 				else:
-# 					body_corpus[word] = body_corpus(word, 0) + 1
-# 	finally:
-# 		f.close()
-#
-# print subj_corpus
-# print body_corpus
-
-# Count the frequency of words in a list
-def word_freq(list):
-    corpus = {}
-    for i in range(len(list)):
-        corpus[list[i]] = list.count(list[i])
-    return sorted(corpus.items(), key=lambda item: item[1], reverse=True)
-
+stop_words = []
+subj_corpus = {}
+body_corpus = {}
 
 # Remove irrelevant words
 # 	Returns 1 if irrevelant, otherwise 0
 def filter(word):
-	# Remove standalone punctuation, special symbols and strings of numbers
+	# Remove if standalone punctuation, special symbols and strings of numbers
 	if (word in string.punctuation) or (word in string.digits):
+		return 1
+	# Remove if word is in list of stop words
+	elif word in stop_words:
 		return 1
 	else:
 		return 0
 
+# Read in stop words from file
+with open('english.stop', 'rU') as stop_file:
+	stop_words = [line.rstrip('\n') for line in stop_file]
+
 # Append words to either subject or body list
 for file in os.listdir(path):
 	f = gzip.open(os.path.join(path, file), 'rb')
-	for line in f:
-		for word in line.split():
-			# Check if word is to be kept
-			if not filter(word):
-				# Line contains subject
-				if line.startswith('Subject:') and (word != 'Subject:'):
-					subj.append(word)
-				# Line contains body
-				else:
-					body.append(word)
+	try:
+		for line in f:
+			for word in line.split():
+				# Check if word is to be kept
+				if not filter(word):
+					# Line contains subject
+					if (line.startswith('Subject:')) and (word != 'Subject:'):
+						# Increment or initialise item
+						subj_corpus[word] = subj_corpus.get(word, 0) + 1
+					# Line contains body
+					else:
+						body_corpus[word] = body_corpus.get(word, 0) + 1
+	finally:
+		f.close()
 
-print word_freq(subj)
-print word_freq(body)
-
-
-
-
-
-
+print sorted(subj_corpus.items(), key=lambda item: item[1], reverse=True)
+print sorted(body_corpus.items(), key=lambda item: item[1], reverse=True)
 
