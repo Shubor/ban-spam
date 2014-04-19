e41c8ceeca2e2c06b7042a40567be165dc094772
Shu <shuhanb@gmail.com>
2014-04-19 17:51:54 +1000

    snooze

--- process.py
+++ process.py
@@ -1,8 +1,8 @@
 import os
 import re
+import csv
 import gzip
-from string import digits
-from string import punctuation
+from string import digits, punctuation
 from collections import Counter
 
 path = 'lingspam-mini600'
@@ -16,12 +16,11 @@ body_corpus = Counter()
 # Check if word is not irrelevant
 # 	Returns 1 if irrevelant, otherwise 0
 def clean(word):
-	# Remove if punctuation, special symbols, number or word is in stop words
+	# Remove if word is punctuation or if word is in stop words
 	if word in punctuation or word.isdigit() or word in stop_words:
 		return 1
 	# Remove strings that contain digits e.g. 'qwoie192kjwe' or '12312-123'
-	# 	or contain punctuation e.g. "n't"
-	elif re.compile('\d').search(word) or re.compile('\W').search(word):
+	elif re.compile('\d').search(word):
 		return 1
 	else:
 		return 0
@@ -38,14 +37,14 @@ for file in os.listdir(path):
 	try:
 		for line in f:
 			# Replace punctuation with space
-			# l = re.sub('[%s]' % re.escape(punctuation), ' ', line)
-			for word in line.split():
+			l = re.sub('[%s]' % re.escape(punctuation), ' ', line)
+			for word in l.split():
 				# Check if word is to be kept
 				if not clean(word) and word not in curr_file_words:
 					curr_file_words.append(word)
 					# Line contains subject
 					if line.startswith('Subject:'):
-						if word != 'Subject:':
+						if word != 'Subject':
 							subj_corpus[word] += 1
 					# Line contains body
 					else:
@@ -56,3 +55,8 @@ for file in os.listdir(path):
 
 print(subj_corpus.most_common(N))
 print(body_corpus.most_common(N))
+
+# Apply tf-idf weighting to selected N terms
+
+# Normalise using cosine normalisation
+# Save data in csv format
