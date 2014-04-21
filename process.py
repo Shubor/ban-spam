5df6ab6c3a1f7147b73ca5f34d1b0bc457506c6c
Shu <shuhanb@gmail.com>
2014-04-21 23:50:30 +1000

    add write to csv method

--- process.py
+++ process.py
@@ -4,7 +4,7 @@ import csv
 import gzip
 import math
 from string import digits, punctuation
-from collections import Counter, defaultdict
+from collections import Counter
 
 path = 'lingspam-mini600'
 
@@ -16,22 +16,20 @@ num_files = 0
 
 # List of subject and body counters for each file
 # 	Stores top N terms in a counter, value=freq in file
-c_subj_legit = []
-c_subj_spam  = []
-c_body_legit = []
-c_body_spam  = []
+c_body_legit, c_body_spam = [], []
+c_subj_legit, c_subj_spam = [], []
 
 # Stores document frequency score for each word
 # 	The number of documents in which the term occurs
-subj_corpus = Counter()
 body_corpus = Counter()
+subj_corpus = Counter()
 
 # Clean:
 # 	Check if word is not irrelevant
 # 	Returns 1 if irrevelant, otherwise 0
 def clean(word):
 	# Remove if word is punctuation or if word is in stop words
-	if word in punctuation or word.isdigit() or word in stop_words:
+	if (word in punctuation) or (word.isdigit()) or (word in stop_words):
 		return 1
 	# Remove strings that contain digits e.g. 'qwoie192kjwe' or '12312-123'
 	elif re.compile('\d').search(word):
@@ -127,10 +125,8 @@ def cosine_norm(term, n_term_in_doc, corpus, logTk):
 	else:
 		return TFIDF(term, n_term_in_doc, logTk)/math.sqrt(denominator)
 
-w_body_legit= []
-w_body_spam = []
-w_subj_legit= []
-w_subj_spam = []
+w_body_legit, w_body_spam = [], []
+w_subj_legit, w_subj_spam = [], []
 
 # Calculate cosine norms
 for document in c_body_legit:
@@ -161,69 +157,19 @@ for document in c_subj_spam:
 # Outputs normalised tf-idf weights into csv files                          #
 #############################################################################
 
-header = [ "f" + str(x) for x in range(1, N+1) ]
-header.append("class")
+# Write CSV:
+#	Write to given csv file
+#	Saves data to file, adding class of file into last column of csv
+def write_csv(file_name, legit, spam):
+	header = [ "f" + str(x) for x in range(1, N+1) ]
+	header.append("class")
+	with open(file_name, "wb") as f:
+		writer = csv.writer(f)
+		writer.writerow(header)
+		writer.writerows( [row + ["nonspam"] for row in legit] )
+		writer.writerows( [row + ["spam"] for row in spam] )
+		f.close()
 
-with open("body.csv", "wb") as f:
-	writer = csv.writer(f)
-	writer.writerow(header)
-	writer.writerows( [row+["nonspam"] for row in w_body_legit] )
-	writer.writerows( [row+["spam"] for row in w_body_spam] )
-	f.close()
-
-with open("subject.csv", "wb") as f:
-	writer = csv.writer(f)
-	writer.writerow(header)
-	writer.writerows( [row+["nonspam"] for row in w_subj_legit] )
-	writer.writerows( [row+["spam"] for row in w_subj_spam] )
-	f.close()
-
-# #############################################################################
-# # Calculate Naive Bayes, assuming normal distribution                       #
-# #############################################################################
-
-# #=====[Probability Functions]=====#
-# mean_body_legit = []
-# mean_body_spam  = []
-# mean_subj_legit = []
-# mean_subj_spam  = []
-
-# # Mean:
-# #	Mean is the average of given values
-# #	E[X] := sum_values / count_values
-# def mean(column):
-# 	return math.fsum([row for row in column]) / len(column)
-
-# for n in range(N):
-# 	# Calculate mean of column fn which are legit
-# 	mean_body_legit.append( mean([row[n] for row in c_body_legit] ))
-# 	mean_body_spam.append(  mean([row[n] for row in c_body_spam]  ))
-
-# 	mean_subj_legit.append( mean([row[n] for row in c_subj_legit] ))
-# 	mean_subj_spam.append(  mean([row[n] for row in c_subj_spam]  ))
-
-# stdev_body_legit = []
-# stdev_body_spam  = []
-# stdev_subj_legit = []
-# stdev_subj_spam  = []
-
-# # Standard deviation:
-# #	SD of X => Sqrt( E[X^2] - (E[X])^2 )
-# def standard_dev(column):
-# 	u = mean(column)
-# 	return math.sqrt( math.fsum([pow(x-u, 2) for x in column]) / (len(column)-1) )
-
-# for n in range(N):
-# 	# Calculate mean of column fn which are legit
-# 	stdev_body_legit.append( standard_dev( [row[n] for row in w_body_legit] ))
-# 	stdev_body_spam.append(  standard_dev( [row[n] for row in w_body_spam ] ))
-# 	stdev_subj_legit.append( standard_dev( [row[n] for row in w_subj_legit] ))
-# 	stdev_subj_spam.append(  standard_dev( [row[n] for row in w_subj_spam ] ))
-
-
-
-
-
-
-
+write_csv("body.csv", w_body_legit, w_body_spam)
+write_csv("subject.csv", w_subj_legit, w_subj_spam)
 
